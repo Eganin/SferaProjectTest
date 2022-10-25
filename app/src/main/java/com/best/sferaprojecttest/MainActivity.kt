41a36713a87bdb1f3e660644bf7c2b404a1e4739
Eganin <egorzaharin12@gmail.com>
2022-10-25 22:37:11 +0500

    add navigation

--- app/src/main/java/com/best/sferaprojecttest/MainActivity.kt
+++ app/src/main/java/com/best/sferaprojecttest/MainActivity.kt
@@ -2,10 +2,15 @@ package com.best.sferaprojecttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.nav_host_fragment)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
     }
 }
